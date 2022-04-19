3ffce7a69695a8742fb16f32d21a5cafbe52fe4d
mohamedtamer0 <mt4452242@gmail.com>
2022-04-19 18:27:16 +0200

    Initial commit

---
+++ MasteringCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
