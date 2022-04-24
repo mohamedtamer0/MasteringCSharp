78e73d8476fd3cafadcf2b981a52e603de9ca695
mohamedtamer0 <mt4452242@gmail.com>
2022-04-24 02:44:36 +0200

    update

--- Pro2/Pro2/Pro2/Program.cs
+++ Pro2/Pro2/Pro2/Program.cs
@@ -10,6 +10,17 @@ namespace Pro2
     {
         static void Main(string[] args)
         {
+            int age = 22;
+            double salary = 1200.99;
+            float height = 3000.5F;
+            byte mybyte = 255;
+
+            Console.WriteLine("your age is : " + age.GetType());
+            Console.WriteLine("your salary is : " + salary.GetType());
+            Console.WriteLine("your height is : " + height.GetType());
+            Console.WriteLine("your mybyte is : " + mybyte.GetType());
+
+
         }
     }
 }
