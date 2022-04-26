c1872b480cec916fcc7102a2a8983d8d7c22dc4e
mohamedtamer0 <mt4452242@gmail.com>
2022-04-27 01:24:43 +0200

    update

--- CallByValue/CallByValue/Program.cs
+++ CallByValue/CallByValue/Program.cs
@@ -9,19 +9,51 @@ namespace CallByValue
     class Program
     {
 
-        static void myData(int num)
+        static int myAge = 25; // Global....
+
+
+        static void Main(string[] args)
+        {
+            //int originalNum = 20;
+            //Console.WriteLine("outside function : " + originalNum);
+            //myData(ref originalNum);
+            //Console.WriteLine("outside function : " + originalNum);
+
+            Console.WriteLine("inside Main function : " + myAge);
+
+
+            int originalNum = 20;
+            Console.WriteLine("outside function : " + originalNum);
+            myResult(out originalNum);
+            Console.WriteLine("outside function : " + originalNum);
+
+
+            myData2();
+        }
+
+
+
+        static void myData(ref int num)
         {
             num += num;
             Console.WriteLine("inside function: " + num);
         }
 
-
-        static void Main(string[] args)
+        static void myResult(out int num)
         {
-            int originalNum = 20;
-            Console.WriteLine("outside function : " + originalNum);
-            myData(originalNum);
-            Console.WriteLine("outside function : " + originalNum);
+            int num2 = 10;
+            num = num2;
+            num *= num;
+            //Console.WriteLine("inside function: " + num);
         }
+
+
+        static void myData2()
+        {
+            int mynum = 10;
+            Console.WriteLine("inside myData2 function: " + mynum);
+        }
+
+
     }
 }
