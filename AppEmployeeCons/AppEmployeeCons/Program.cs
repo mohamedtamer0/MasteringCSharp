51b2621ccd517ded3cf01639cc9c3617b867909c
mohamedtamer0 <mt4452242@gmail.com>
2022-04-30 22:38:34 +0200

    update

--- AppEmployeeCons/AppEmployeeCons/Program.cs
+++ AppEmployeeCons/AppEmployeeCons/Program.cs
@@ -6,6 +6,29 @@ using System.Threading.Tasks;
 
 namespace AppEmployeeCons
 {
+
+    class Person
+    {
+        string name;
+        int age;
+        string city;
+
+        public Person(string _name, int _age, string _city)
+        {
+            name = _name;
+            age = _age;
+            city = _city;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Your Name is : " + name + " age : " + age + " city : " + city);
+        }
+
+    }
+
+
+
     class Program
     {
 
@@ -18,6 +41,11 @@ namespace AppEmployeeCons
         static void Main(string[] args)
         {
             Program obj = new Program();
+            Program obj2 = new Program();
+            Person obj3 = new Person("Mohamed",25,"Giza");
+            obj3.PrintInfo();
+            Person obj4 = new Person("Osama",30,"cairo");
+            obj4.PrintInfo();
         }
     }
 }
