513ca58c959d09f2674f57db813d4634db9c246e
mohamedtamer0 <mt4452242@gmail.com>
2022-05-01 01:53:33 +0200

    update

--- AppPloymorphisim/AppPloymorphisim/Program.cs
+++ AppPloymorphisim/AppPloymorphisim/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace AppPloymorphisim
 {
+
+    class Person
+    {
+        public string name = "Person";
+        public virtual void run()
+        {
+            Console.WriteLine("running.........");
+        }
+    }
+
+    class Student:Person
+    {
+        public string name = "Mohamed";
+        public override void run()
+        {
+            Console.WriteLine("Student running.........");
+        }
+    }
+
+    class Employee : Person
+    {
+        public override void run()
+        {
+            Console.WriteLine("Employee running.........");
+        }
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            Person per;
+
+            per = new Student();
+            per.run();
+            Console.WriteLine("Student name : " + per.name);
+
+            per = new Employee();
+            per.run();
         }
     }
 }
---
+++ AppSealed/AppSealed/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSealed
+{
+
+    sealed class Person
+    {
+        public void walk()
+        {
+            Console.WriteLine("walk!");
+        }
+    }
+
+    class Student :Person
+    {
+        public void smile()
+        {
+            Console.WriteLine("smile!");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+            obj.smile();
+            obj.walk();
+        }
+    }
+}
