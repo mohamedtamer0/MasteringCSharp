48d964bef2308181a159483e00b2b36836d6aa2e
mohamedtamer0 <mt4452242@gmail.com>
2022-05-02 00:57:30 +0200

    update

---
+++ AppNameSpace/AppNameSpace/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohamed
+{
+    class Person
+    {
+        public void PrintInfo()
+        {
+            Console.WriteLine("nameSpace Mohamed!");
+        }
+    }
+}
+
+namespace Tamer
+{
+    class Person
+    {
+        public void PrintInfo()
+        {
+            Console.WriteLine("nameSpace Tamer!");
+        }
+    }
+}
+
+
+namespace AppNameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mohamed.Person mper = new Mohamed.Person();
+            mper.PrintInfo();
+
+            Tamer.Person tper = new Tamer.Person();
+            tper.PrintInfo();
+        }
+    }
+}
