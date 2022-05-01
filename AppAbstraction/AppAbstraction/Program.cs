adf15c90a217b3eb9e2546e3de0d1095eea40e5a
mohamedtamer0 <mt4452242@gmail.com>
2022-05-01 23:44:02 +0200

    update

---
+++ AppAbstraction/AppAbstraction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAbstraction
+{
+
+   abstract class Person
+    {
+        public abstract void name();
+    }
+
+    class Student : Person
+    {
+        public override void name()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person per;
+            per = new Student();
+            per.name();
+        }
+    }
+}
---
+++ AppInterface/AppInterface/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppInterface
+{
+
+    interface Person
+    {
+        void fullName();
+    }
+
+    interface Person2
+    {
+        void age();
+    }
+
+    class Student : Person, Person2
+    {
+        public void fullName()
+        {
+            Console.WriteLine("Hello Student");
+        }
+
+        public void age()
+        {
+            Console.WriteLine("Student age");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per;
+            Student stu = new Student();
+            stu.fullName();
+            stu.age();
+            
+        }
+    }
+}
