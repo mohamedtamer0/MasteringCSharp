e5296f9e2cccf257abf215ba7a94a9f75a75b98c
mohamedtamer0 <mt4452242@gmail.com>
2022-05-02 03:41:50 +0200

    update

---
+++ Collections_Dictionary/Collections_Dictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new Dictionary<string, string>();
+            names.Add("1","Mohamed");
+            names.Add("2", "Tamer");
+            names.Add("3", "Ahmed");
+            names.Add("4", "Osama");
+            names.Add("5", "Ali");
+
+            foreach (KeyValuePair<string,string> x in names)
+            {
+                Console.WriteLine("Key :" + x.Key + " " + "Value: " + x.Value);
+            }
+
+            /*
+             * Sorted - Dictionary
+             */
+
+            Console.WriteLine("-------------------");
+
+            var names2 = new SortedDictionary<int, string>();
+            names2.Add(2, "Mohamed");
+            names2.Add(3, "Tamer");
+            names2.Add(5, "Ahmed");
+            names2.Add(1, "Osama");
+            names2.Add(4, "Ali");
+
+            foreach (KeyValuePair<int, string> x in names2)
+            {
+                Console.WriteLine("Key :" + x.Key + " " + "Value: " + x.Value);
+            }
+        }
+    }
+}
---
+++ Collections_Sorted_List/Collections_Sorted_List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Sorted_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new SortedList<string, string>();
+            names.Add("3", "Mohamed");
+            names.Add("5", "Tamer");
+            names.Add("2", "Ahmed");
+            names.Add("4", "Osama");
+            names.Add("1", "Ali");
+
+            foreach (KeyValuePair<string, string> x in names)
+            {
+                Console.WriteLine("Key :" + x.Key + " " + "Value: " + x.Value);
+            }
+        }
+    }
+}
