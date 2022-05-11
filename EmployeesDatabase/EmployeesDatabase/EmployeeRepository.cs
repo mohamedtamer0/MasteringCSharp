e4ad677d7bbfc99699c2abb7602a76bb8514d312
mohamedtamer0 <mt4452242@gmail.com>
2022-05-11 21:33:53 +0200

    update

---
+++ EmployeesDatabase/EmployeesDatabase/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
+
+namespace EmployeesDatabase
+{
+
+    class EmployeeRepository
+    {
+
+        DbProviderFactory factory;
+        string provider;
+        string connectionString;
+        public EmployeeRepository()
+        {
+            provider = ConfigurationManager.AppSettings["provider"];
+            connectionString = ConfigurationManager.AppSettings["connectionString"];
+            factory = DbProviderFactories.GetFactory(provider);
+        }
+
+        public List<Employee> GetAll()
+        {
+            var employees = new List<Employee>();
+            using(var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                var command = factory.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = "Select * From Employees";
+                using(DbDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        employees.Add(new Employee
+                        {
+                            id = (int)reader["Id"],
+                            FirstName = (string)reader["FirstName"],
+                            LastName = (string)reader["LastName"]
+                        });
+                    }
+                }
+            }
+            return employees;
+        }
+    }
+}
